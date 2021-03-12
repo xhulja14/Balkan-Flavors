de37097ec07bafd77cec09c3f9a9e8f479be6095
xhulja14 <xhulja14@gmail.com>
2021-03-12 00:26:54 +0000

    Update the recipe in the database

--- app.py
+++ app.py
@@ -114,6 +114,16 @@ def add_recipe():
 
 @app.route("/edit_recipe/<recipe_id>", methods=["GET", "POST"])
 def edit_recipe(recipe_id):
+    if request.method == "POST":
+        submit = {
+            "recipe_name": request.form.get("recipe_name"),
+            "recipes_name": request.form.get("recipes_name"),
+            "recipe_description": request.form.get("recipe_description"),
+            "created_by": session["user"]
+        } 
+        mongo.db.recipes.update({"_id": ObjectId(recipe_id)},submit)
+        flash("Recipe Successfully Updated")
+
     recipe = mongo.db.recipes.find_one({"_id": ObjectId(recipe_id)})
     menu = mongo.db.menu.find().sort("recipe_name", 1)
     return render_template("edit_recipe.html", recipe=recipe, menu=menu)
